7663701d585924af66dfdbaecffa714c95c44b4a
Aurelien <aurelien.boncenne@gmail.com>
2017-09-27 11:12:08 +0200

    ajout fichier bdd, fonction et session

---
+++ src/inc/functions.php
@@ -0,0 +1,55 @@
+<?php
+function encrypt($pure_string, $encryption_key) {
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+    $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+
+    return $encrypted_string;
+
+}
+
+
+function decrypt($encrypted_string, $encryption_key) {
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+    $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, $encrypted_string, MCRYPT_MODE_ECB, $iv);
+
+    return $decrypted_string;
+
+}
+function reconnect_from_cookie(){
+    if(session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+    if(isset($_COOKIE['remember']) && !isset($_SESSION['auth']) ){
+        require_once 'db.php';
+        if(!isset($db)){
+            global $db;
+        }
+        $remember_token = $_COOKIE['remember'];
+        $parts = explode('==', $remember_token);
+        $user_id = $parts[0];
+        $req = $pdo->prepare('SELECT * FROM user WHERE id = ?');
+        $req->execute([$user_id]);
+        $user = $req->fetch();
+        if($user){
+            $expected = $user_id . '==' . $user->remember_token . sha1($user_id . 'ratonlaveurs');
+            if($expected == $remember_token){
+                session_start();
+                $_SESSION['user'] = $user;
+                setcookie('remember', $remember_token, time() + 60 * 60 * 24 * 7);
+            } else{
+                setcookie('remember', null, -1);
+            }
+        }else{
+            setcookie('remember', null, -1);
+        }
+    }
+}
+?>
