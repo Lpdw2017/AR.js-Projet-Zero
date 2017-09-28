d7564f3e2e9f128e5843c05e0cad51433ed67d33
lpdw <lpdw@MC-G-ST-C121-3.local>
2017-09-28 10:54:20 +0200

    ajout page inscription, connexion et deconnexion

---
+++ src/deconnexion.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+<div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col md-12">
+          <?php include('logout.php');?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php require 'footer.php'; ?>
---
+++ src/inscription.php
@@ -0,0 +1,15 @@
+<?php require 'header.php'; ?>
+<div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col md-12">
+          <?php include('register.php');?>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <?php require 'footer.php'; ?>
