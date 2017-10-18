c3df1bebb12894fff5341a65803794e9a171112c
lpdw <lpdw@MC-G-ST-C121-3.local>
2017-10-18 15:19:54 +0200

    suppression fichier bancal

--- header.php
+++ header.php
@@ -8,25 +8,17 @@ require('inc/db.php');
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Realite augmentee</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <!--Theme custom css -->
         <link rel="stylesheet" href="assets/css/style.css">
         <!--Theme Responsive css-->
-        <link rel="stylesheet" href="assets/css/responsive.css" />
-        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/examples/vendor/aframe/build/aframe.min.js"></script>
-        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/build/aframe-ar.js"></script>
+        <script src="lib/three.js/three.min.js"></script>
+        <script src="lib/three.js/OBJLoader.js"></script>
+        <script src="lib/AR.js/ar.js"></script>
 </head>
-
-
           <nav class="navbar navbar-inverse">
-
-
-
             <ul id="menu_horizontal" class="nav navbar-nav" style="left:50%;">
             <li><a href="index.php">Accueil</a></li>
             <li><a href="inscription.php">Inscription</a></li>
@@ -37,7 +29,7 @@ require('inc/db.php');
 </nav>
 
 
-    
+
 
 
 
