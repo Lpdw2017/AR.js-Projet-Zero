f33770051f31c22864304af661d5e572dd793d1e
lpdw <lpdw@MC-G-ST-C121-3.local>
2017-10-18 17:04:32 +0200

    modification header + index

--- header.php
+++ header.php
@@ -3,37 +3,27 @@ require('inc/session.php');
 require('inc/db.php');
 ?>
 <!doctype html>
-<html class="" lang="fr" >
+<html lang="fr" >
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Realite augmentee</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
-        <!--Theme Responsive css-->
-        <link rel="stylesheet" href="assets/css/responsive.css" />
-        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/examples/vendor/aframe/build/aframe.min.js"></script>
-        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.5.0/aframe/build/aframe-ar.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 
 <div class="container">
-          <nav class="navbar navbar-inverse">
-            <ul id="menu_horizontal" class="nav navbar-nav" style="left:50%;">
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="inscription.php">Inscription</a></li>
-            <li><a href="connexion.php">Connexion</a></li>
+          <nav class="row navbar navbar-inverse">
+            <ul id="menu_horizontal" class="nav navbar-nav">
+              <li><a href="index.php">Accueil</a></li>
+              <li><a href="inscription.php">Inscription</a></li>
+              <li><a href="connexion.php"><i class="fa fa-user-circle" aria-hidden="true"></i>  Connexion</a></li>
+              <li><a href="deconnexion.php">Déconnexion</a></li>
             </ul>
-</nav>
-
-
-
-
-
-
+          </nav>
 <body>
--- index.php
+++ index.php
@@ -1,32 +1,19 @@
 <?php
-require('inc/session.php');
-require('inc/db.php');
+  require 'header.php';
 ?>
-        <?php
-          require 'header.php';
-        ?>
-        <? /*<section id="slideshow"> */?>
-
-        <div class="row heading-content">
-               <h1>La Réalité Augmentée</br> débarque dans ta fac !</h1>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-          					<h3>Projet 0 :</h3></br></br>
-          					<p>Voici une description de notre projet : </br></br></br>
-          					Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat
-          					imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum
-          					oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe
-          					discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos
-          					ex isdem foveis cerneret emergentes
-          					</P>
-            </div>
-        	<div class="col-md-6">
-        			<a href="camera.php"><img src="./images.jpeg"></img>
-        	</div>
-      </div>
-        <? /*  </section> */?>
-
+  <div class="row heading-content">
+      <h1>La Réalité Augmentée</br> débarque dans ta fac !</h1>
+  </div>
+  <div class="row">
+      <h3>Projet 0 :</h3></br>  <i class="fa fa-camera-retro fa-5x"></i> </br>
+        <p>Voici une description de notre projet : </br></br></br>
+            inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat
+          	imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum
+          	oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe
+          	discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos
+          	ex isdem foveis cerneret emergentes
+        </p>
+    </div>
 <?php
  require 'footer.php';
  ?>
