b7c542a9282a995554bff2985210f379066dd92f
lpdw <lpdw@MC-G-ST-C121-7.local>
2017-10-18 15:34:02 +0200

    modif stephane

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!--Footer-->
 <footer id="footer" class="footer">
- 
+
 
         <div class="social">
           <a href="#"><i class="fa fa-facebook"></i></a>
@@ -10,7 +10,7 @@
         </div>
 
 </footer>
-
+</div>
 
 <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
 <script src="assets/js/vendor/bootstrap.min.js"></script>
