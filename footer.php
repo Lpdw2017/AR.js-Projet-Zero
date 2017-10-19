1cc9f9c6d8d67e28a3ad7af94fd19bb805141bfd
lpdw <lpdw@MC-G-ST-C121-3.local>
2017-10-19 12:21:32 +0200

    css footer et index

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
+
 <!--Footer-->
 <footer id="footer" class="footer">
-        <div class="social">
+        <div class="row social">
+          <a href="https://www.faclab.org"><img class="img-thumbnail" id="img-fab" src="assets/fac-lab-gennevilliers.jpg"></img></a>
           <a href="#"><i class="fa fa-facebook"></i></a>
           <a href="#"><i class="fa fa-twitter"></i></a>
           <a href="#"><i class="fa fa-linkedin"></i></a>
