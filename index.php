c13801e82e7608d739859185468dfe9b7e7e7b0d
lpdw <lpdw@MC-G-ST-C121-5.local>
2017-10-19 10:28:49 +0200

    modification texte index.php

--- index.php
+++ index.php
@@ -5,15 +5,29 @@
       <h1>La Réalité Augmentée</br> débarque dans ta fac !</h1>
   </div>
   <div class="row">
-      <h3>Projet 0 :</h3></br>  <i class="fa fa-camera-retro fa-5x"></i> </br>
-        <p>Voici une description de notre projet : </br></br></br>
-            inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat
-          	imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum
-          	oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe
-          	discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos
-          	ex isdem foveis cerneret emergentes
-        </p>
+    <div class="col-md-offset-2 col-md-8">
+      <h3>The Faclab Quest</h3></br>
+        <p>Hola voyageur temporel !</br></br></br></p>
+            <p class="text-center">Prêt à vivre la plus incroyable quête & à accéder ainsi aux portes du très convoité Faclab & de son incroyable trésor ?</br>
+
+            Alors inscris-toi, connecte-toi & plonge-toi dans un univers encore inexploré ! </br>
+            L’aventure t’attend !</p>
+
+          <p class="text-center"> Il te faudra d’abord t’équiper:</br>
+            de courage,</br>
+            de cellules grises</br>
+            mais surtout d’un smartphone Android !</br></br></p>
+
+            <p>Chaque indice te mènera à un endroit précis où il te faudra trouver un marqueur de ce type:</br></br></p>
+
+            <p class="text-center"><img src="hiro.png"><br/></br></br></br>
+
+
+            Son utilité ? Entre dans la quête & découvre-la !</p></br></br></br>
+
+          <div class= "col-md-offset-5"><img src="./PtInterro.gif"></div>
     </div>
+  </div>
 <?php
  require 'footer.php';
  ?>
