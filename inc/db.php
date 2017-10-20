5c99a6c2a664bdb815632e06ea25b3419e36e591
Aurelien <aurelien.boncenne@gmail.com>
2017-10-20 10:16:01 +0200

    correctif docker

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,8 @@
 <?php
 $host ='localhost';
-$db_name ='arjs';
-$user ='root';
-$password ='root';
+$db_name ='arjs_kwry';
+$user ='arjs_kwry';
+$password ='lpdw2017';
 
 try {
 $db = new PDO("mysql:dbname=" .$db_name.";host=" .$host, $user, $password );
